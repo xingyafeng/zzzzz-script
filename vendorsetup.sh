85b204dc375811693f194238bae7e59b49b37c47
yafeng <root@126.com>
2015-05-16 19:01:54 +0800

    add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+##############################################
+#
+#
+#		funtion : main script 入口
+#		author  : yafeng
+#		time    : 2014-9-9
+#
+#############################################
+
+#!/bin/bash
+
+CHIPHD_ANDROID_SCRIPT_PATH=device/softwinner/zzzzz-script
+
+source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_setup.sh
