322cf22ffe62e4c963f5d61f063556979c540c61
yafeng <root@126.com>
2015-05-16 19:02:27 +0800

    add chiphd_show.sh

---
+++ chiphd_show.sh
@@ -0,0 +1,89 @@
+###########################################################
+###
+###				tools functions
+###
+###	
+### date 	: 2013-12-05 19:55
+### author  : yafeng
+###
+###########################################################
+
+#!/bin/bash
+
+### color red
+function show_vir
+{
+	if [ "$1" ]
+	then
+	#	echo "---------------------"
+		for ret in "$@"; do
+			echo -e -n "\e[1;31m$ret \e[0m"
+		done
+		echo
+	fi
+}
+
+### color green 
+function show_vig
+{
+	if [ "$1" ]
+	then
+	#	echo "---------------------"
+		for ret in "$@"; do
+			echo -e -n "\e[1;32m$ret \e[0m"
+		done
+		echo
+	fi
+}
+
+### color yellow 
+function show_viy
+{
+	if [ "$1" ]
+	then
+	#	echo "---------------------"
+		for ret in "$@"; do
+			echo -e -n "\e[1;33m$ret \e[0m"
+		done
+		echo
+	fi
+}
+
+### color purple 
+function show_vip
+{
+	if [ "$1" ]
+	then
+	#	echo "---------------------"
+		for ret in "$@"; do
+			echo -e -n "\e[1;35m$ret \e[0m"
+		done
+		echo
+	fi
+}
+
+### color dark green 
+function show_vid
+{
+	if [ "$1" ]
+	then
+	#	echo "---------------------"
+		for ret in "$@"; do
+			echo -e -n "\e[1;36m$ret \e[0m"
+		done
+		echo
+	fi
+}
+
+
+function csoftwinner
+{
+	local T	
+	T=$(gettop)
+	cd "$T/device/softwinner"
+	#show_vig $T
+}
+
+###########################
+#### 	file EOF		###
+###########################
