f66c8fefb08e017a82028464849bb9b3074e2d10
yafeng <root@126.com>
2015-05-18 15:57:23 +0800

    add chiphd_setup.sh

---
+++ chiphd_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_show.sh
+source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_command.sh
+source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_adb_shell.sh
+source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_make_android.sh
+source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_make_lichee.sh
+source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_make_ota.sh
