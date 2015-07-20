c2284216f2957a3824a1dd33e1cb7633c1fcdfe5
yafeng <root@126.com>
2015-07-20 11:14:30 +0800

    modify init_script.sh,add

--- init_script.sh
+++ init_script.sh
@@ -1,17 +1,24 @@
-#############
+#######################################
 #
 #
+#	author: yafeng
+#	date: 2015--7-20
 #
 #
-#
-#
-#############
+######################################
 
 
 #!/bin/bash
 
 script_path=/a/0ps/zzzzz-script
 
+time_date=`date +%m%d`
+td=$da/$time_date
+
 if [ -d $script_path ];then
 	source $script_path/vendorsetup.sh
+fi
+
+if [ ! -e "$da/$time_date" ];then
+	mkdir $da/$time_date
 fi
