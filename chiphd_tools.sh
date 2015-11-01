a38ff7068c6c33bc51ab2f849f75f10a15c6bccf
yafeng <box@chiphd.com>
2015-11-01 19:43:38 +0800

    restore change_ps, change_usb_mode  add get_modify_file

--- chiphd_tools.sh
+++ chiphd_tools.sh
@@ -11,60 +11,76 @@ function DEBUG()
 	[ "$_DEBUG" == "on" ] && $@ || :
 }
 
-ssh_path=~/.ssh
-
 function change_ssh
 {
-	if [ -L $ssh_path ];then	
+	local ssh_path=~/.ssh
+	local project_name=$1
+
+	if [ -L $ssh_path ];then
 		rm $ssh_path
 	fi
 	
-	if [ $1 = "aw" ];then
-		ln -s /home/abc/ssh_aw/ /home/abc/.ssh
+	if [ $project_name = "aw" ];then
+		ln -s ~/ssh_${project_name} $ssh_path
 	elif [ $1 = "yfk" ];then
-		ln -s /home/abc/ssh_inphic/ /home/abc/.ssh
+		ln -s /home/abc/ssh_${project_name} $ssh_path
 	elif [ $1 = "dymt" ];then
-		ln -s /home/abc/ssh_diyomate/ /home/abc/.ssh
+		ln -s /home/abc/ssh_${project_name} $ssh_path
 	else
 		show_vir "please input args[0] ..."
+		show_vir "change_ssh_ (aw, yfk, dymt )"
 	fi
 }
 
 function change_repo
 {
-	if [ -L /a/3pr/repo/repo ]; then
-		rm /a/3pr/repo/repo
-	fi
+	local repo_path=$pr/repo
+	local project_name=$1
 	
-	if [ $1 = "aw" ];then
-		ln -s /a/3pr/repo/repo_allwinner /a/3pr/repo/repo
-	elif [ $1 = "yfk" ];then
-		ln -s /a/3pr/repo/repo.aliyunos.yfk /a/3pr/repo/repo
-	elif [ $1 = "dymt" ];then
-		ln -s /a/3pr/repo/repo.aliyunos.diyomate /a/3pr/repo/repo
+	if [ -L $repo_path/repo ]; then
+		rm $repo_path/repo
+	fi
+
+	if [ $project_name = "aw" ];then
+		ln -s $repo_path/repo_${project_name} $repo_path/repo
+	elif [ $project_name = "yfk" ];then
+		ln -s $repo_path/repo.aliyunos.${project_name} $repo_path/repo
+	elif [ $project_name = "dymt" ];then
+		ln -s $repo_path/repo.aliyunos.${project_name} $repo_path/repo
+	else
+		show_vir "please input args[0] ..."
+		show_vir "change_repo_ (aw, yfk, dymt )"
 	fi
 }
 
 function change_ps
 {
-	change_ssh $1
-	change_repo $1
+	if [ "$1" ];then
+		change_ssh $1
+		change_repo $1
+	else
+		show_vir "please input args[0] ..."
+		show_vir "change_ps_ (aw, yfk, dymt )"
+	fi
 }
 
 function switch_usb_mode
 {
-	if [ $1 = "d" ];then
+	local usb_mode=$1
+
+	if [ $usb_mode == "device" ];then
 		adb shell cat sys/bus/platform/devices/sunxi_usb_udc/usb_device
-	elif [ $1 = "h" ];then
+	elif [ $usb_mode == "host" ];then
 		adb shell cat /sys/bus/platform/devices/sunxi_usb_udc/usb_host
 	fi
-
 }
 
 function setgitconfig
 {
-	if [ "$1" ];then
-		git config --global  user.name $1
+	local your_name=$1
+
+	if [ "$your_name" ];then
+		git config --global  user.name $your_name
 	else
 		git config --global  user.name yafeng
 	fi
@@ -79,32 +95,62 @@ function setgitconfig
 	git config --global alias.lg "log --date=short --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %C(green)%s %C(reset)(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit"
 }
 
-function get_modify_file
+function get_android_and_lichee__modify_file
 {
-	local this_android_path=$(gettop)
-	local modify_file_path=
+	local hold_project=$2
+	local now_date=`date +%y%m%d`
 
-	modify_file_path=`repo status | awk '$1=="project" && NF > 2 {prj=$2} $1 !~ "d" && NF==2 && $2 !~ "preApk/system/app-lib" {printf "android/%s%s\n",prj,$2 }'`
+	show_vip $date_tar
+	cd ..
+	tar zcf $td/modify.tar.gz $1
 
-	if [ "$modify_file_path" ];then
+	if [ -f $td/modify.tar.gz ];then
 
-		cd ..
-		tar zcf $td/modify.tar.gz $modify_file_path
-
-		if [ -f $td/modify.tar.gz ];then
-
+		if [ $hold_project == "android" ];then
 			if [ -d $td/android ];then
 				rm $td/android -rf
 			fi
 
 			tar zxf $td/modify.tar.gz -C $td
-
-			if [ $? -eq 0 ];then
-				rm $td/modify.tar.gz
+			mv $td/modify.tar.gz $td/modify_${hold_project}_20${now_date}.tar.gz
+		elif [ $hold_project == "lichee" ];then
+			if [ -d $td/lichee ];then
+				rm $td/lichee -rf
 			fi
+
+			tar zxf $td/modify.tar.gz -C $td
+			mv $td/modify.tar.gz $td/modify_${hold_project}_20${now_date}.tar.gz
 		fi
 	fi
-	cd $this_android_path
+}
+
+function get_modify_file
+{
+	local this_sdk_path=`pwd`
+	local android_modify_file_path=
+	local lichee_modify_file_path=
+
+	if [ "$this_sdk_path" ];then
+		this_sdk_path=${this_sdk_path##*/}
+	#	show_vig $this_sdk_path
+	else
+		show_vig "this path do not eaxit, please checkout input args ..."
+	fi
+
+	if [ $this_sdk_path == "android" ];then
+		android_modify_file_path=`repo status | awk '$1=="project" && NF > 2 {prj=$2} $1 !~ "d" && NF==2 && $2 !~ "preApk/system/app-lib" {printf "android/%s%s\n",prj,$2 }'`
+
+		if [ "$android_modify_file_path" ];then
+			get_android_and_lichee__modify_file "$android_modify_file_path" $this_sdk_path
+		fi
+	elif [ $this_sdk_path == "lichee" ];then
+		lichee_modify_file_path=`repo status | awk '$1=="project" && NF >2 {prj=$2} NF==2 && $1 !~ "d" {printf "lichee/%s%s\n", prj, $2}'`
+
+		if [ "$lichee_modify_file_path" ] ;then
+			get_android_and_lichee__modify_file "$lichee_modify_file_path" $this_sdk_path
+		fi
+	fi
+	cd $this_sdk_path
 }
 
 function mount_diyomate
