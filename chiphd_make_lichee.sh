ac4e2183bae1d99c338a6964d1269a4a47e11669
yafeng <box@chiphd.com>
2015-12-07 15:35:26 +0800

    modiyf chiphd_make_lichee.sh

--- chiphd_make_lichee.sh
+++ chiphd_make_lichee.sh
@@ -12,15 +12,15 @@ show_ui()
 
 function make-lichee
 {
-#	thisPath=$(pwd) && thisPath=${thisPath%/*} && thisPath=${thisPath##*/}	
+#	local thisPath=$(pwd) && thisPath=${thisPath%/*} && thisPath=${thisPath##*/}
 	local lichee_name=lichee
-
-	if [ ! -z $thisPath ];then
+	if [ -z $thisPath ];then
 		show_ui $lichee_name $thisPath
 	else
-		show_vir  thisPath is null!	
+		show_vir  "thisPath is null!	"
 	fi
 
+	echo $thisPath
 	if [ "`is_make_project`" = "true" ];then
 		### 判断pro_name 是否已经选择对应的平台	
 		if [ -z $pro_name ];then	
@@ -33,9 +33,9 @@ function make-lichee
 		show_viy "pro_name = $pro_name"
 
 		echo
-		if [ $pro_name = "dolphin" ];then
+		if [ "$pro_name" == "dolphin" ];then
 			thisPath=$dolphin44
-		elif [ $pro_name = "eagle" ];then
+		elif [ "$pro_name" == "eagle" ];then
 			thisPath=$eagle44
 		fi
 	fi
@@ -81,15 +81,20 @@ function make-lichee
 
 function make-uboot
 {
-#	thisPath=$(pwd) && thisPath=${thisPath%/*} && thisPath=${thisPath##*/}
-
+#	local thisPath=$(pwd) && thisPath=${thisPath%/*} && thisPath=${thisPath##*/}
 	local uboot_name=u-boot
 	local old_pwd=$(pwd)
 	local uboot_path=brandy/u-boot-2011.09
 
-	show_ui $uboot_name $thisPath
+	if [ ! -z $thisPath ];then
+		show_ui $uboot_name $thisPath
+	else
+		show_vir  "thisPath is null!	"
+	fi
 
-	if [ "`is_make_project`" = "true" ];then
+
+
+	if [ "`is_make_project`" == "true" ];then
 		show_vir "请按照下面提示输入对应的编译平台: dolphin 和 eagle" 
 		show_vir "-----------------------------------------------"
 		echo -n "Please follow the tips below input " && show_vig dolphin or eagle
@@ -101,9 +106,9 @@ function make-uboot
 			show_vir "--------------------------------touch $pro_name"
 		fi
 
-		if [ $pro_name = "dolphin" ];then
+		if [ "$pro_name" == "dolphin" ];then
 			thisPath=$dolphin44
-		elif [ $pro_name = "eagle" ];then
+		elif [ "$pro_name" == "eagle" ];then
 			thisPath=$eagle44
 		fi
 	fi
@@ -117,13 +122,14 @@ function make-uboot
 			if make sun8iw6p1_config;then
 				show_vir "--> choose sun8iw6p1_config end ..."
 				echo
-				if make -j32;then
-					show_vip "--> make h8 uboot end."
+				if make spl;then
+					show_vip "---> make h8 boot0 end."
 					echo
-					if make boot0;then
-						show_vip "--> make h8 boot0 end."
+					if make -j32;then
+						show_vip "--> make h8 uboot end."
+						echo
 					fi
-				fi
+				fi	
 			fi
 		fi
 	;;
@@ -133,11 +139,13 @@ function make-uboot
 			show_vir "--> disclean end ..."
 			if make sun8iw7p1_config;then
 				show_vir "--> choose sun8iw7p1_config end ..."
-				if make -j32;then
-					show_vip "--> make h3 uboot end."
+				echo
+				if make spl;then
+					show_vir "---> make h3 boot0 end."
 					echo
-					if make boot0;then
-						show_vip "--> make h3 boot0 end."
+					if make -j32;then
+						show_vip "--> make h3 uboot end."
+						echo
 					fi
 				fi
 			fi
