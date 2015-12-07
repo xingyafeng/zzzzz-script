7eb7a7719cfb384be4e8a4cd29c6690f82c70ab3
yafeng <box@chiphd.com>
2015-12-07 15:39:53 +0800

    add test sh

--- chiphd_setup.sh
+++ chiphd_setup.sh
@@ -8,3 +8,4 @@ source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_make_lichee.sh
 source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_make_ota.sh
 source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_tools.sh
 source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_alias.sh
+source $CHIPHD_ANDROID_SCRIPT_PATH/chiphd_test_shell.sh
---
+++ chiphd_test_shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function test-pwd
+{
+	local test_path=`pwd`
+	show_vir "test_path = $test_path"
+}
