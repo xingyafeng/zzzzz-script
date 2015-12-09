27c9bc70f71bb6c3834d4aceccac5bb69c25bb16
yafeng <box@chiphd.com>
2015-12-09 17:07:08 +0800

    modify chiphd_tools.sh chiphd_command.sh

--- chiphd_command.sh
+++ chiphd_command.sh
@@ -106,85 +106,6 @@ function ccode
 	cd $scode_path
 }
 
-### login ssh server 
-function box
-{
-	ssh boxbuilder@192.168.1.23
-}
-
-function gittt
-{
-	ssh git@192.168.1.20
-}
-
-function droid05
-{
-	ssh droid05@192.168.1.23
-}
-
-function hbc
-{
-	ssh hbc@192.168.1.20
-}
-
-function diyomate
-{
-	ssh diyomate@192.168.1.11
-}
-
-#### mount server
-mount_box()
-{
-	mount_project boxbuilder 23 123456
-}
-
-mount_droid05()
-{
-	mount_project droid05 23 123456
-}
-
-mount_hbc()
-{
-	mount_project hbc 20 hbc
-}
-
-mount_yj()
-{
-	mount_project yj 22 123456
-}
-
-function mount-chiphd
-{
-	mount_box
-	mount_droid05
-	mount_hbc
-	mount_yj
-}
-
-### mount base function
-function mount_project
-{
-	local server_name=$1
-	local server_no=$2
-	local server_key=$3
-	
-	if [ $# -lt 3 ];then
-		show_vir "please input three arsg."
-		show_vir "eg : mount_project droid05 23 123456"
-		show_vir "please eg: mount_project  服务器名称 + 服务器地址 + 密码"
-		return 1;
-	fi
-	
-	if [ ! -d /mnt/$server_name ];then
-		sudo mkdir /mnt/$server_name
-		if [ $? -eq 0 ];then
-			sudo mount -t cifs -o username=$server_name,password=$server_key,rw,uid=abc,gid=abc //192.168.1.$server_no/$server_name /mnt/$server_name
-		fi	
-	else
-		sudo mount -t cifs -o username=$server_name,password=$server_key,rw,uid=abc,gid=abc //192.168.1.$server_no/$server_name /mnt/$server_name
-	fi
-}
-
 umount_box()
 {
 	umount_project boxbuilder/
@@ -232,35 +153,6 @@ function umount_project
 }
 
 
-### open file
-open-file()
-{
-	if [ $# -eq 0 ];then		
-		nautilus .			
-	else
-		nautilus $1
-	fi
 
-	if [ "$1" = "--help" ];then
-		show_vip "----------help---------------"	
-	fi
-}
 
-### find and grep
-find-file()
-{
-	if [ "$1" ];then
-		find . -type f -name "$1" -print
-	else
-		show_vip "please add only one arg, eg:find-file + string"
-	fi
-}
 
-grep-file() 
-{
-	if [ "$1" ]; then
-		find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.java' -o -name '*.xml' -o -name '*.sh' -o -name '*.mk' -o -name '*.rc' -o -name '*.cfg' -o -name 'Makefile' -o -name 'Kconfig' -o -name '*.sh' \) -print0 | xargs -0 grep --color -n $@
-	else
-		show_vip "what do you want to grep ?"
-	fi
-}
