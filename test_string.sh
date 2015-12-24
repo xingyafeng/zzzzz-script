0fa79025d93a79eae8762af62d99f6e7f57a589e
yafeng <box@chiphd.com>
2015-12-24 15:43:38 +0800

    modify test_string.sh

--- test_string.sh
+++ test_string.sh
@@ -2,31 +2,12 @@
 
 test-string()
 {
-	var=chiphd
+	local var=chiphd
 
 	# get string length
-	length=${#var}	
+	local length=${#var}	
 
 	show_vir $length
 }
 
 
-test-let()
-{
-	no1=100
-	no2=120
-
-
-	result=$((no1+no2))
-	result=$[ no1 + no2 ]
-	echo $result 
-
-	echo "--------------------------符号 运算--------------------------"
-	let result=no1+no2
-	result_expr=`expr 3 + 5`
-
-	echo $result 
-	echo $result_expr
-
-	echo "----------------------------bc----------------------------------"
-}
