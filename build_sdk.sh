202c55cee264adfaf50ba9d50001e3db216da0da
yafeng <box@chiphd.com>
2016-05-07 21:29:46 +0800

    　k86l add GaodeCarMap

--- build_sdk.sh
+++ build_sdk.sh
@@ -64,7 +64,7 @@ prefect_name="$file_project/$file_name/$file_version"
 ### clone system app
 commond_app=(FactoryTest CarEngine CarHomeBtn CarSystemUpdateAssistant CarPlatform GaodeMap KwPlayer UniSoundService)
 k86a_app=(CarUpdateDFU CarBack CarRecord GaodeNavigation GpsTester BaiduNavigation AnAnEDog CldNavi NewsmyNewyan NewsmyRecorder NewsmySPTAdapter)
-k86l_app=(CarUpdateDFU CarBack CarRecordDouble CarRecordUsb StormVideo GpsTester XianzhiDSA CarConfig FileCopyManager)
+k86l_app=(CarUpdateDFU CarBack CarRecordDouble CarRecordUsb StormVideo GpsTester XianzhiDSA CarConfig FileCopyManager GaodeCarMap)
 k86s_app=(CarUpdateDFU CarRecordDouble CarRecordUsb GpsTester BaiduNavigation AnAnEDog StormVideo CldNavi NewsmyNewyan NewsmyRecorder NewsmySPTAdapter)
 k26a_app=(CarRecord GpsTester BaiduNavigation)
 k26s_app=(CarRecordDouble CarRecordUsb GpsTester BaiduNavigation StormVideo)
