4e463e624fd6cb31293037d252fb344db889467a
yafeng <xingyf@yunovo.cn>
2016-05-30 09:59:17 +0800

    remove blank

--- chiphd_make_ota.sh
+++ chiphd_make_ota.sh
@@ -6,7 +6,7 @@ ota_target_file=out/target/product/*/obj/PACKAGING/target_files_intermediates
 function make_target
 {
 	if get_uboot;then
-		show_vip "--> get uboot end."	
+		show_vip "--> get uboot end."
 		if make target-files-package;then
 			show_vip "--> make target file end."
 		fi
@@ -18,13 +18,13 @@ function make-target
 	if make_target;then
 		if [ -d $ota_target_file ];then
 			mv $ota_target_file/*.zip ./old_target_files.zip
-		fi		
+		fi
 	fi
 }
 
 function make-inc
 {
-	if make otapackage_inc;then		
+	if make otapackage_inc;then
 		show_vip "--> make inc end."
 	fi
 }
@@ -61,7 +61,7 @@ function make-sdk
 
 #	echo "android_path = $android_path "
 #	echo "lichee_path = $lichee_path"
-	
+
 	cd $lichee_path
 	make-uboot
 	echo
@@ -94,19 +94,19 @@ function make-inc-more
 {
 	echo $ANDROID_TOP
 	echo $TARGET_PATH
-	
+
 	### create inc patch
 	if [ ! -d $EAGLE_PATH ];then mkdir $EAGLE_PATH && show_vip "--> mkdir file ok.";fi
-	
-	if [ -d $TARGET_PATH ];then 
+
+	if [ -d $TARGET_PATH ];then
 		local target_name
 		local i=1
 		for target_name in "${menu_target_name[@]}";do
 		#	show_vip $target_name
-		
+
 			if [ -f $TARGET_PATH/$target_name ];then
-				if [ -f $ANDROID_TOP/old_target_files.zip ];then 
-					rm old_target_files.zip 
+				if [ -f $ANDROID_TOP/old_target_files.zip ];then
+					rm old_target_files.zip
 					show_vip "--> rm old_target_files ."
 				fi
 
@@ -114,10 +114,10 @@ function make-inc-more
 				if cp $TARGET_PATH/$target_name  $ANDROID_TOP/old_target_files.zip ;then
 					show_vip "--> copy old_target_files.zip ."
 					if make-inc;then
-						### copy inc 
-						if [ -d $EAGLE_PATH ];then		
+						### copy inc
+						if [ -d $EAGLE_PATH ];then
 							cp $OUT/*.zip $EAGLE_PATH/inc-v$i.0-v$(($i+1)).0.zip && show_vip "copy inc-xxx.zip ."
-						fi	
+						fi
 					fi
 				fi
 				show_vir "-------------------------"
