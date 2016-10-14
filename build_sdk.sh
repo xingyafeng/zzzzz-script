5a98dd5a0a57f50b2089aa7f94528a4e8a5afc4d
hewenfeng <hewenfeng@yunovo.cn>
2016-10-14 09:25:27 +0800

    add he-All-Series. in is_yunovo_server

--- build_sdk.sh
+++ build_sdk.sh
@@ -371,7 +371,7 @@ function is_yunovo_branch()
 function is_yunovo_server()
 {
     local hostN=`hostname`
-    local serverN=(s1 s2 s3 s4 happysongs ww)
+    local serverN=(s1 s2 s3 s4 happysongs ww he-All-Series.)
     local isServer=false
 
     for n in ${serverN[@]}
