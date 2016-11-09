82b6350055743ab1ea8adf69945bc148a01f58fe
yafeng <xingyf@yunovo.cn>
2016-11-09 16:26:27 +0800

    update create_manifest.sh, e.g auto_create_manifest_xml number

--- create_manifest.sh
+++ create_manifest.sh
@@ -1,48 +1,84 @@
 #!/usr/bin/env bash
 
-function creat_manifest_xml()
+apps_xml=fs/apps.xml
+
+function auto_create_manifest_xml()
 {
-    local manifest_path=fs/manifest.xml
-    local string="<project name=\"yunovo_packages/CarEngine\" path=\"yunovo_packages/apps/CarEngine\" revision=\"master\" />"
+    local count=$1
+
+    local main_xml=tools/main.xml
+    local default_xml=fs/default.xml
+    local string="    <project name=\"yunovo_packages/CarEngine\" path=\"yunovo_packages/apps/CarEngine\" />"
+    local project_end="</manifest>"
+
+    if [ "$count" ];then
+        echo
+        show_vip "    creat manifest start ..."
+        echo
+    else
+        echo "count is null ! e.g. auto_create_manifest_xml count ..."
+        return 1
+    fi
 
     if [ ! -d fs ];then
         mkdir fs
     fi
 
-    if [ -f $manifest_path ];then
-        rm $manifest_path && touch $manifest_path
+    if [ -f $apps_xml ];then
+        rm $apps_xml && touch $apps_xml
     else
-        touch $manifest_path
+        touch $apps_xml
     fi
 
-    for ((i=1; i<=116; i++))
+    for ((i=1; i<=$count; i++))
     do
-        if [ -f $manifest_path ];then
-            echo $string >> $manifest_path
+        if [ -f $apps_xml ];then
+            echo $string >> $apps_xml
         fi
     done
 
-    if [ -f $manifest_path ];then
+    if [ -f $apps_xml ];then
         modify_packages_name
     fi
+
+    if [ -f $default_path ];then
+        cat $main_xml > $default_xml
+
+        while read line;do
+            echo "    $line" >> $default_xml
+        done < $apps_xml
+
+        echo >> $default_xml
+        echo $project_end >> $default_xml
+    fi
+
+    echo
+    show_vip "    creat manifest end ..."
+    echo
 }
 
 
 function modify_packages_name()
 {
     local count=1
-    local tmp=fs/tmp.txt
-    local manifest_path=fs/manifest.xml
+    local appfs=fs/apps.txt
+
+    if [ -f $appfs ];then
+        :
+    else
+        echo "apps.txt is not exist !"
+        return 1
+    fi
 
     while read app;do
-        echo "---> app = $app"
+        show_vir "    ---> app = $app"
 
         while read p;do
             if [ "`echo $p | grep CarEngine`" ];then
-                sed -i "${count}s/CarEngine/$app/g" $manifest_path
+                sed -i "${count}s/CarEngine/$app/g" $apps_xml
                 let count++
                 break
             fi
-        done < $manifest_path
-    done < $tmp
+        done < $apps_xml
+    done < $appfs
 }
