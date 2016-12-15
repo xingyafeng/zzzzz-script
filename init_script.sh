cd391f4adc85418bcda35b4fa43037fe8564ef41
yafeng <xingyf@yunovo.cn>
2016-12-15 11:29:39 +0800

    s5 server support ssh-update-script

--- init_script.sh
+++ init_script.sh
@@ -30,7 +30,7 @@ source $script_path/chiphd_script_update.sh
 ### misc
 source $script_path/chiphd_misc.sh
 
-if [ $hostN == "f1" -o $hostN == "s1" -o $hostN == "s2" -o $hostN == "s3" -o $hostN == "s4" -o $hostN == "happysongs" -o $hostN == "siawen" -o $hostN == "ww" ];then
+if [ $hostN == "f1" -o $hostN == "s1" -o $hostN == "s2" -o $hostN == "s3" -o $hostN == "s4" -o $hostN == "s5" -o $hostN == "happysongs" -o $hostN == "siawen" -o $hostN == "ww" ];then
     update-chiphd-script
     mkdir_data_folder
     set_alias
