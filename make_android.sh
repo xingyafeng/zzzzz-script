b880285bb650b3c9914ba0863caceee2ad0d6510
yafeng <xingyf@yunovo.cn>
2016-12-29 19:57:09 +0800

    单独处理app的android.mk

--- make_android.sh
+++ make_android.sh
@@ -1175,6 +1175,27 @@ function handler_custom_config()
     fi
 }
 
+function handler_android_mk()
+{
+    local app=$1
+    local OLDPWD=`pwd`
+    local yunovo_app_path=yunovo/packages/apps
+    local sh=auto_create_android_mk.sh
+
+    if [ $build_prj_name == "k28s_LD-A107C" ];then
+
+        if [ "$app" == "CarConfig" ];then
+            cd $yunovo_app_path/$app > /dev/null
+
+            if [ -f $sh ];then
+                ./$sh $app
+            fi
+
+            cd $OLDPWD > /dev/null
+        fi
+    fi
+}
+
 function down_load_yunovo_source_code()
 {
     local prj_name=`get_project_name`
@@ -1453,6 +1474,7 @@ function main()
     if [ "`is_check_lunch`" != "no lunch" -a "$build_update_code" == "true" ];then
         copy_customs_to_android
         handler_custom_config
+        handler_android_mk CarConfig
     else
         _echo "current directory is not android ! gettop is null !"
         return 1
@@ -1536,6 +1558,88 @@ EOF
 
         if [ -f $diff_manifest_log ];then
             rm -rf $diff_manifest_log
+        fi
+    fi
+}
+
+### 自动创建android.mk
+function auto_create_android_mk()
+{
+    local android_mk_file_name=Android.mk
+    local armeabi_so=armeabi
+    local armeabi_v7a_so=armeabi-v7a
+    local curr_apk_name=$1
+
+    local jni_lib="LOCAL_PREBUILT_JNI_LIBS := \\"
+    local build_prebuild="include \$(BUILD_PREBUILT)"
+
+    if [ $# -eq 1 ];then
+        :
+    else
+        _echo "Please e.g auto_create_android_mk  xxx.apk ..."
+        return 1
+    fi
+
+    (cat << EOF) > ./$android_mk_file_name
+LOCAL_PATH := \$(call my-dir)
+
+EOF
+
+if false;then
+    if [ "$curr_apk_name" ];then
+        curr_apk_name="${curr_apk_name/%.apk/}"
+    else
+        return 1
+    fi
+fi
+
+    (cat << EOF) >> ./$android_mk_file_name
+include \$(CLEAR_VARS)
+LOCAL_MODULE := $curr_apk_name
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_SRC_FILES := \$(LOCAL_MODULE).apk
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MULTILIB := 32
+
+EOF
+    if [ "`unzip -l ${curr_apk_name}.apk | awk '$(NF) ~ /lib\/armeabi-v7a\/.*.so$/ {print $(NF)}'`" ];then
+        unzip -l ${curr_apk_name}.apk | awk '$(NF) ~ /lib\/armeabi-v7a\/.*.so$/ {print $(NF)}' > $zz_script_path/${armeabi_v7a_so}.txt
+    elif [ "`unzip -l ${curr_apk_name}.apk | awk '$(NF) ~ /lib\/armeabi\/.*.so$/ {print $(NF)}'`" ];then
+        unzip -l ${curr_apk_name}.apk | awk '$(NF) ~ /lib\/armeabi\/.*.so$/ {print $(NF)}' > $zz_script_path/${armeabi_so}.txt
+    else
+        echo $build_prebuild >> ./$android_mk_file_name
+    fi
+
+    if [ -f $zz_script_path/${armeabi_v7a_so}.txt ];then
+        echo $jni_lib >> ./$android_mk_file_name
+        while read lib_path;do
+            echo "    @$lib_path \\" >> ./$android_mk_file_name
+        done < $zz_script_path/${armeabi_v7a_so}.txt
+
+        echo >> ./$android_mk_file_name
+        echo $build_prebuild >> ./$android_mk_file_name
+
+        rm $zz_script_path/${armeabi_v7a_so}.txt
+    elif [ -f $zz_script_path/${armeabi_so}.txt ];then
+
+        echo $jni_lib >> ./$android_mk_file_name
+
+        while read lib_path;do
+            echo "    @$lib_path \\" >> ./$android_mk_file_name
+        done < $zz_script_path/${armeabi_so}.txt
+
+        echo >> ./$android_mk_file_name
+        echo $build_prebuild >> ./$android_mk_file_name
+
+        rm $zz_script_path/${armeabi_so}.txt
+    else
+        if [ -f $android_mk_file_name ];then
+            sed -i '/LOCAL_MULTILIB := 32/d' $android_mk_file_name
+        else
+            __echo "Android.mk not found, please check it !"
+            return 1
         fi
     fi
 }
