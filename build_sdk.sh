eb84d36aa1e1c061a5ac80a535f3e5f333a9acfd
yafeng <xingyf@yunovo.cn>
2017-02-21 17:00:46 +0800

    add k86s7_NM-N810 project checkout TxzVoice txzing2.0
    Change-Id: I21056c11bccc58c12f799771fe2b249cc369fd31

--- build_sdk.sh
+++ build_sdk.sh
@@ -2118,7 +2118,7 @@ function handler_branch_for_TxzVoice()
 
     case $build_prj_name in
 
-        k86mx1_MB-M8S)
+        k86s7_NM-N810 | k86mx1_MB-M8S)
 
             TxzVoice_branch=txzing2.0
             ;;
