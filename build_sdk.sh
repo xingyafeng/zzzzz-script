94f176b37e0db48b11b2b6e106600175e6815901
yafeng <xingyf@yunovo.cn>
2017-02-27 12:32:47 +0800

    modify receiver email address 281220263@qq.com
    Change-Id: I031c15ea1175df3cc5b2cbc2948614d469d45daa

--- build_sdk.sh
+++ build_sdk.sh
@@ -3034,7 +3034,7 @@ function main()
     fi
 
     if [ "`is_root_version $build_type`" == "true" ];then
-        email_receiver="514779897@qq.com"
+        email_receiver="281220263@qq.com"
         email_content="make root project successful ..."
     else
         email_receiver="android_software@yunovo.cn"
