534bcf3c5a0e2f908534f0e5b4059ac71ad11963
yafeng.xing <yafeng.xing@tcl.com>
2020-06-15 19:44:38 +0800

    Classified script file.
    Change-Id: Id7e709d90abda23aeb62e28a671a3f1692942dae

--- jenkins/jenkins_init.sh
+++ jenkins/jenkins_init.sh
@@ -1,41 +1,53 @@
 #!/usr/bin/env bash
 
-## 初始化公共的函数
+# 初始化公共函数
 . "`dirname $0`"/init_script.sh
 
+# 初始化差异函数
 case ${shellfs##*/} in
 
-    build_sdk.sh)
+    build_sdk.sh) #编译Android
         . "`dirname $0`"/jenkins/yunovo/yunovo_make_func.sh
 
         make_version=Android
         ;;
 
-    make_android.sh|makefs.sh|replace_logo.sh|make_ota.sh|build_ota.sh|repo_diffmanifests.sh|make_so.sh)
+    make_android.sh|makefs.sh) #编译Android
         . "`dirname $0`"/jenkins/yunovo/yunovo_make_android.sh
 
         make_version=Android
         ;;
 
-    mm.sh|rsync.sh|rdiff_backup.sh|build_app.sh|gerrit_cherry_pick.sh|demo.sh|notify.sh|apk_release.sh|rom_release.sh|apk_resign.sh)
-        :
-        ;;
-
-    gitrepositories.sh|pref.sh|updatemanifest.sh|updatebranch.sh|check_app.sh)
-        :
-        ;;
-
-    make_nxos.sh)
-        . "`dirname $0`"/jenkins/nxos/yunovo_make_nxos.sh
-        ;;
-
-    mk_aliphone.sh|mk_aliphone4mt6737t.sh)
+    mk_aliphone.sh|mk_aliphone4mt6737t.sh) #编译阿里
         . "`dirname $0`"/jenkins/yunos/yunovo_make_aliphone.sh
 
         make_version=Yunos
         ;;
 
-    teleweb2zip.sh|download_mirror.sh)
+    make_nxos.sh) #编译应用
+        . "`dirname $0`"/jenkins/nxos/yunovo_make_nxos.sh
+        ;;
+
+    make_ota.sh|build_ota.sh|make_so.sh) #编译OTA SO
+        . "`dirname $0`"/jenkins/yunovo/yunovo_make_android.sh
+        ;;
+
+    repo_diffmanifests.sh|replace_logo.sh) #构建差异
+        . "`dirname $0`"/jenkins/yunovo/yunovo_make_android.sh
+        ;;
+
+    apk_release.sh|rom_release.sh) #发布APK和ROM版本
+        :
+        ;;
+
+    check_app.sh|build_app.sh|apk_resign.sh) #APK重签名
+        ;;
+
+    notify.sh|demo.sh|mm.sh|rsync.sh|rdiff_backup.sh|updatemanifest.sh) #misc
+        :
+        ;;
+
+    gitrepositories.sh|pref.sh|updatebranch.sh|gerrit_cherry_pick.sh) #git
         :
         ;;
 
