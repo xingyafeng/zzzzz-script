53456ea1328e8c286a8176c78650b98e01f675d9
yafeng.xing <yafeng.xing@tcl.com>
2020-09-04 13:55:28 +0800

    test args
    Change-Id: I98f62d32c89834bd601c01cf437523291945c879

--- test/test_func.sh
+++ test/test_func.sh
@@ -14,3 +14,44 @@ function test_remove_space()
         echo ${old_v}
     fi
 }
+
+# 测试参数提取
+function test_get_args() {
+
+    for parameter in $@
+    do
+        echo $@
+        echo
+        echo 'args: '${parameter}
+        echo '----------------'
+        echo
+
+        start=$(expr match "${parameter}" '-\|--')
+        option=${parameter:${start}}
+
+        echo '1.start  : '${start}
+        echo '2.option : '${option}
+        echo '3.${parameter:0:1} : '${parameter:0:1}
+        log print '---- end'
+
+        if [[ ${start} -gt 0 ]]; then
+            start=$(expr match "${parameter}" '-\|--')
+            option=${parameter:${start}}
+        elif [[ "${parameter:0:1}" != '-' ]];then
+
+            if [[ ${index} -eq 0 ]];then
+                project=${parameter};
+            fi
+
+            if [[ ${index} -eq 1 ]];then
+                operator=${parameter};
+            fi
+
+            log debug "project = ${project} ; operator= ${operator}"
+            ((index++))
+        else
+            echo "!!unvalid parameter '$parameter' !!\n"
+            return 0
+        fi
+    done
+}
