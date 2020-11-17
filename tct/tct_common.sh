576654d8d38df66ff9137e5013460a844957df7e
yafeng.xing <yafeng.xing@tcl.com>
2020-11-17 15:30:46 +0800

    modify filter()
    Change-Id: Ibf06cd327423bf6d4cc94fef5ccf110d00c9ed8b

--- tct/tct_common.sh
+++ tct/tct_common.sh
@@ -197,3 +197,73 @@ function handle_tct_custom() {
         mv vendor/tct/source/qcn/Android.mk vendor/tct/source/qcn/Android.mk_bak
     fi
 }
+
+# 过滤不进行编译的项目或分支
+function filter() {
+
+    case ${project_name} in
+        sm7250-r0-seattletmo-dint) # seattletmo R
+            case ${GERRIT_PROJECT} in
+                genericapp/gcs_HiddenMenu)
+                    case ${GERRIT_BRANCH} in
+                        Gcs_HiddenMenu_Common_mtk)
+                            echo true
+                        ;;
+
+                        *)
+                            echo false
+                        ;;
+                    esac
+                ;;
+
+                *)
+                    echo false
+                ;;
+            esac
+        ;;
+
+        sm6125-r0-portotmo-dint) # portotmo R
+            case ${GERRIT_PROJECT} in
+                genericapp/gcs_HiddenMenu)
+                    case ${GERRIT_BRANCH} in
+                        Gcs_HiddenMenu_Common_mtk)
+                            echo true
+                        ;;
+
+                        *)
+                            echo false
+                        ;;
+                    esac
+                ;;
+
+                *)
+                    echo false
+                ;;
+            esac
+        ;;
+
+        mt6762-tf-r0-v1.1-dint) # Tokyo Lite TMO R
+            case ${GERRIT_PROJECT} in
+                genericapp/gcs_HiddenMenu)
+                    case ${GERRIT_BRANCH} in
+                        Gcs_HiddenMenu_Common)
+                            echo true
+                        ;;
+
+                        *)
+                            echo false
+                        ;;
+                    esac
+                ;;
+
+                *)
+                    echo false
+                ;;
+            esac
+            ;;
+
+        *)
+            echo false
+        ;;
+    esac
+}
