3f1ff89eb30dd1f8929e50442afa3a1b165f26c3
yafeng.xing <yafeng.xing@tcl.com>
2021-01-06 10:11:18 +0800

    add docker-search-demo
    Change-Id: I164596c7bc425509a47ae1fb21cee7bd32e9e2aa

--- linux/linux_utils.sh
+++ linux/linux_utils.sh
@@ -209,6 +209,24 @@ function set_alias()
     alias l='ls -CF'
 }
 
+#
+# Simple script that will display docker repository tags.
+#
+# Usage:
+#   $ docker-show-repo-tags.sh ubuntu centos
+function docker-search-demo() {
+
+    echo 'NAME'
+    for Repo in $* ; do
+      curl -s -S "https://registry.hub.docker.com/v2/repositories/library/$Repo/tags/" | \
+        sed -e 's/,/,\n/g' -e 's/\[/\[\n/g' | \
+        grep '"name"' | \
+        awk -F\" '{print $4;}' | \
+        sort -fu | \
+        sed -e "s/^/${Repo}:/"
+    done
+}
+
 #####################################################
 ##
 ##  函数: enhance_mkdir_folder
