949e6a08f0b69cce1efa4ecd04f55e456f32c27c
yafeng.xing <yafeng.xing@tcl.com>
2021-04-07 16:10:10 +0800

    update mma.sh
    Change-Id: Ie6688f07e91d825104d61f20c0bfa9cbe9f39919

--- mma.sh
+++ mma.sh
@@ -2,7 +2,6 @@
 
 # Common utilities, variables and checks for all build scripts.
 set -o errexit
-set -o nounset
 set -o pipefail
 
 # TODO 临时的、短期解决方案的、或者足够好但不够完美的代码
@@ -18,6 +17,8 @@ function main() {
     trap 'ERRTRAP ${LINENO} ${FUNCNAME} ${BASH_LINENO}' ERR
 
     local target=${1:-}
+
+    source build/envsetup.sh
 
     for t in ${target[@]} ; do
        Command mma -j$(nproc) ${t}
