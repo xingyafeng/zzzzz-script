ba0a075f290f01c245de29048d257bab380612ad
yafeng.xing <yafeng.xing@tcl.com>
2021-05-28 10:12:25 +0800

    update init_script.sh
    Change-Id: Ice2916fabd511fb428cec5d03f4e858c93f63120

--- init_script.sh
+++ init_script.sh
@@ -26,10 +26,11 @@ fi
 
 #------------------------------------------------------------------------------ 准备环境
 
+#echo "fatal: ${tmpfs}/yf.lock': File exists."
 if [[ "$(is_build_server)" == "true" ]];then
+
     if [[ -f ${tmpfs}/yf.lock ]];then
-#        echo "fatal: ${tmpfs}/yf.lock': File exists."
-    :
+        :
     else
         #　下载或更新zzzzz-script脚本仓库
         if [[ $(is_connect_netwrok 'http://sz.gerrit.tclcom.com:8080') == "true" ]];then
